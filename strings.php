4e6cbda577eea168a82260de06933f95c17cb2eb
Mamba1099 <mambakaranja240@gmail.com>
2023-08-31 11:05:09 +0300

    fuctions reverses the string

--- strings.php
+++ strings.php
@@ -8,9 +8,13 @@
 <body>
     <?php
         // returns the length of a string
-        echo strlen("Hello, World");
+        echo strlen("Hello, World"); // output 12
 
-        
+        // functions counts the words in a string
+        echo str_word_count("Hello, World!"); //output 2
+
+        // fuctions reverses a string
+        echo strrev("Hello, World!"); //output !dlroW ,olleH 
     ?>
 </body>
 </html>
