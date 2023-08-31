1fc058bf82f2952b6d46f5494d26111f6b2250a9
Mamba1099 <mambakaranja240@gmail.com>
2023-08-31 14:20:44 +0300

    looping thorugh a block of code a specified number of times

---
+++ loops/for_loop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for Loops</title>
+</head>
+<body>
+    <?php
+        //The for loop is used when you know in advance how many times the script should run.
+        // Syntax
+        // for (init counter; test counter; increment counter) {
+        //      code to be executed for each iteration; }
+
+        for ($h = 0; $h <= 10; $h++) {
+            echo ("number is: $h <br>");
+        }
+    ?>
+</body>
+</html>
