b4b93aa6b8e1805138ddeeb3a8c1e3215ab13a4f
Mamba1099 <mambakaranja240@gmail.com>
2023-08-31 15:21:14 +0300

    defining a function in php

---
+++ functions/w_msg.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // block os code used repeatedly in a program
+    //function functionName() {
+    //   code to be executed; } 
+    function print_msg () {
+        echo ("I am doing fine!!!");
+    }
+    print_msg() // call the function
+    ?>
+</body>
+</html>
