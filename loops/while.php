25580026a748d0b557e1f249858574f31b866cc4
Mamba1099 <mambakaranja240@gmail.com>
2023-08-31 13:02:48 +0300

    execute a block of code once the condition is true

---
+++ loops/while.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // while loop
+    //it executes a block of code one the condition is true
+    //  while (condition is true) {
+    //        code to be executed; }
+    
+    $X = 1; // Starting line
+
+    while ($x <= 10) {
+        echo ("number is: $x <br>"); // <br> places number in new line
+        
+        $x++; // increments the value by 1
+    }
+    ?>
+</body>
+</html>
