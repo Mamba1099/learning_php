3f85a0eb5fdd4677bf60eb5bd7c4063f161be226
Mamba1099 <mambakaranja240@gmail.com>
2023-08-31 16:15:26 +0300

    defining a function with multiple arguement

--- functions/funct_arg.php
+++ functions/funct_arg.php
@@ -8,13 +8,22 @@
 <body>
     <?php
     // arguements are specified after the function name
-    // a function can have amny arguements
+    // a function can have manny arguements
     function family_name ($last_name) {
         echo ("$last_name yujiro.<br>");
     }
     family_name("Martin");
     family_name("Kevin");
     family_name("Max");
+
+    // function with multiple arguement
+    function person ($fname, $lname, $year) {
+        echo ("$fname $lname was born in $year <br>");
+    }
+    person("vic", "Refsnes", "1999");
+    person("Mark", "Refsnes", "2001");
+    person("Mike", "Refsnes", "2003");
+
     
     ?>
 </body>
