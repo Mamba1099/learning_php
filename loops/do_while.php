a468721f34c99ed23fb010ec0427c1741edea924
Mamba1099 <mambakaranja240@gmail.com>
2023-08-31 13:17:13 +0300

    looping thorugh a block of code and then repeats it once the condition is true

---
+++ loops/do_while.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // The do...while loop - Loops through a block of code once, and then repeats the loop as long as the specified condition is true.
+    //  Syntax
+    //   do {
+    //      code to be executed;
+    //     while (condition is true); }
+    $t = 'ouch!';
+
+    do {
+        echo ("$t <br>"); // increment ouch 9 times to satisfy the condition
+        $t ++;
+    }
+    while ($t <10);
+    ?>
+</body>
+</html>
