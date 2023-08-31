8c6ba09d6e3b7acec854672068f17ba1d1b40afc
Mamba1099 <mambakaranja240@gmail.com>
2023-08-31 15:04:52 +0300

    using continue with for and while loop

--- loops/continue.php
+++ loops/continue.php
@@ -8,11 +8,19 @@
 <body>
     <?php
         // statement continue breaks one iteration in the loop and continue with the next iteration in the group
+        // using for loop
         for ($g = 0; $g < 10; $g++) {
             if ($g == 4) { // when true it skips the number 4 
                 continue;
             }
             echo ("$g <br>");
+        }
+        // using while loop
+        $t = 1;
+
+        while ($t <= 10) {
+            echo ("number is: $t <br>");
+            $t++;
         }
     ?>
 </body>
